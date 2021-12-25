0e88a021aa2df6c12c91f4e12bb6109fe9c2eb6a
sstttrano <91220857+sstttrano@users.noreply.github.com>
2021-12-26 01:23:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Nature</title>
+	<link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<header class="header">
+			<div class="header__container _container">
+				<a href="#" class="header__logo"><span>Gold</span>Fish</a>
+				<div class="header__menu-icon"><img src="img/menu.png" alt=""></div>
+				<nav class="header__menu">
+					<ul class="header__list">
+						<li><a href="#" class="header__link">Home</a></li>
+						<li><a href="#about" class="header__link">About</a></li>
+						<li><a href="#" class="header__link">Our job</a></li>
+						<li><a href="#" class="header__link">Gallerey</a></li>
+						<li><a href="#" class="header__link">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<section class="head">
+			<div class="head__container _container">
+				<div class="head__content">
+					<div class="head__row">
+						<h1 class="head__title">Let your soul rest</h1>				
+						<p class="head__text">Give yourself a break from the race of life and look into the world of your peace of mind</p>				
+						<button class="head__button">Leave a request or</button>
+						<a href="#about" class="head__link">Read more</a>						
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="about" id="about">
+			<div class="about__container _container">
+				<div class="about__content">
+					<div class="about__image">
+						<div class="img"></div>
+					</div>
+					<div class="about__text">
+						<div class="about__title">
+							<div class="about__title-item"><p>About us</p></div>
+							<h2>Goldfish Global</h2>
+						</div>
+						<p>We are the ones who can help you cool down in the race of life and allow yourself to take a little break</p>
+						<p>We will help you find a holiday home in the most atmospheric place in the world, the view from the window of which you will definitely like</p>
+						<p>We entered the market in the now distant, distant 2014 During this time, we have analyzed most of our visitors and identified several important components of a successful holiday</p>
+						<a href="#" class="about__button">Get a rest</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section class="work">
+			<div class="work__container">
+				<div class="work__content">
+					<div class="work__text">
+						<div class="work__title">
+							<div class="work__item"><p>What we do</p></div>
+							<h2>Lorem impsum</h2>
+						</div>
+						<div class="work__text-content">
+							<p><span>1</span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, culpa. Placeat nobis optio tempora beatae numquam soluta! Suscipit explicabo ea nemo saepe cumque, maiores vel velit magnam. Sapiente, placeat at?</p>
+							<p><span>2</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti qui voluptatum voluptate impedit? Officia sequi ipsam perferendis placeat natus consectetur consequatur ut accusantium tempore voluptatum eligendi at, reiciendis ad tenetur.</p>
+							<p><span>3</span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur facilis dolorem iure nulla, vero suscipit vel totam! Alias at eum reiciendis iure harum quas quidem quisquam ex animi nisi. Iure.</p>
+							<div class="work__link">
+								<a href="#">Read more...</a>
+							</div>
+						</div>
+					</div>
+					<div class="work__image"></div>
+				</div>
+			</div>
+		</section>
+		<div class="photoTitle">
+			<div class="photoTitle__container _container">
+				<div class="photoTitle__row">
+					<div class="photoTitle__title">
+						<p>Photos</p>
+					</div>	
+					<div class="photoTitle__text">
+						<p>Our gallery</p>
+					</div>	
+					<div class="bg"></div>		
+				</div>
+			</div>
+		</div>
+		<section class="gallery">
+			<div class="gallery__container _container">
+				<div class="gallery__content">
+					<div class="gallerey__item _item-1">
+					</div>
+					<div class="gallerey__item _item-2">
+					</div>
+					<div class="gallerey__item _item-3">
+					</div>
+					<div class="gallerey__item _item-4">
+					</div>
+					<div class="gallerey__item _item-5">
+					</div>
+					<div class="gallerey__item _item-6">
+					</div>
+				</div>
+			</div>
+		</section>
+		<div class="contacts">
+			<div class="contacts__container _container">
+				<p class="contacts__title">
+					You can contact us here
+				</p>
+				<div class="contacts__row">
+					<div class="contacts__form-div">
+						<form class="contacts__form" action="">
+							<div class="form__header">
+								<input type="text" class="form__mail" placeholder="email">
+								<input type="text" class="form__name" placeholder="name">
+							</div>
+							<textarea type="text" class="form__message" placeholder="message"></textarea>
+							<button type="submit">Submit</button>
+						</form>
+					</div>
+					<div class="contacts__contacts">
+						<div class="contacts-container">
+							<a href="tel:0909">+7 (123) 456-78-90</a>
+							<a href="mailto: asdsd@sdf.com">examplemail@mail.com</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="BG"></div>
+	</div>
+</body>
+</html>
